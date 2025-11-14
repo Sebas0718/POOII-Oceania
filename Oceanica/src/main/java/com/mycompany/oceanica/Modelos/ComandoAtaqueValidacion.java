e291287f0e3a1fdc0b32a12aefcbe54932510e94
Sebastián Chaves <schaves0707@gmail.com>
2025-11-13 22:06:40 -0600

    Se implementan los ataques Swirl Tornado

--- Oceanica/src/main/java/com/mycompany/oceanica/Modelos/ComandoAtaqueValidacion.java
+++ Oceanica/src/main/java/com/mycompany/oceanica/Modelos/ComandoAtaqueValidacion.java
@@ -4,6 +4,9 @@
  */
 package com.mycompany.oceanica.Modelos;
 
+import com.mycompany.Interfaz.Celda;
+import com.mycompany.Interfaz.InterfazPrincipal;
+
 /**
  *
  * @author xsusk
@@ -15,9 +18,35 @@ public class ComandoAtaqueValidacion {
 
     }
     
-    public static boolean fueraDeAlcanceThreeNumbers(int num1, int num2, int num3){
+    public static boolean fueraDeAlcanceThreeNumbers(int num1, int num2, int num3) {
         return (num1 > 0 || num1 < 9) && (num2 > 0 || num2 < 9) && (num3 > 0 || num3 < 9);
-        
+
     }
     
+    public static boolean validarRemolinoSeleccionado(int numRemolino) {
+
+        // int cantidadRemolinos = buscarRemolinosEnTablero(InterfazPrincipal interfaz);
+        // if (numRemolino > cantidadRemolinos){
+        //     return false;   
+        // }
+        return true;
+    }
+
+
+    private int buscarRemolinosEnTablero(InterfazPrincipal interfaz) {
+        
+        Celda[][] celdas = interfaz.getCeldas();
+        int cantidadRemolinos = 0;
+        for (Celda[] filaCeldas : celdas) {
+            for (int j = 0; j < celdas[0].length; j++) {
+                if (filaCeldas[j].isTieneRemolino()) {
+                    cantidadRemolinos++;
+                }
+            }
+        }
+        return cantidadRemolinos;
+    }
+    
+
+
 }
